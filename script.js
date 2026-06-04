00b9189fd83934d808853f2360e8dbfa30583755
Leon Pllana <leon.pllana@lecom-ks.com>
2026-06-04 19:38:07 +0200

    Add menu toggle functionality and close menu on link click

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("menu").classList.remove("active");
+  });
+});
