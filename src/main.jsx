b2171971e8c69d3b45620b8f5852b088a9b9fc5e
Leon Pllana <135324050+pllanaio@users.noreply.github.com>
2026-06-29 14:59:59 +0200

    Convert React entry to Vite module syntax

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-const React = require("react");
-const { createRoot } = require("react-dom/client");
-const App = require("./App.jsx").default;
-
-require("../style.css");
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "../style.css";
 
 createRoot(document.getElementById("root")).render(
-  React.createElement(React.StrictMode, null, React.createElement(App))
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
